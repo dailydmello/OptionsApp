bd393309b4e8e47d3941c224505f13ac6387dc63
dmelloe <dailydmello@gmail.com>
2018-07-24 20:29:34 -0700

    Connected all the buttons,text fields,labels to controller

--- OptionsApp/OptionsApp/Controllers/CalculatorViewController.swift
+++ OptionsApp/OptionsApp/Controllers/CalculatorViewController.swift
@@ -11,8 +11,33 @@ import UIKit
 
 class CalculatorViewController: UIViewController{
     
+    
+    @IBOutlet weak var symbolTextField: UITextField!
+    @IBOutlet weak var currentPriceLabel: UILabel!
+    
+    @IBOutlet weak var getPriceButton: UIButton!
+    
+    @IBOutlet weak var optionStrategySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var strikePriceTextField: UITextField!
+    @IBOutlet weak var numofContractsTextField: UITextField!
+    @IBOutlet weak var expiryDateTextField: UITextField!
+    @IBOutlet weak var costLabel: UILabel!
+    
+    @IBOutlet weak var graphButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func getPriceButtonTapped(_ sender: UIButton) {
+        print("get price button tapped")
+    }
+    
+    @IBAction func strategySelected(_ sender: UISegmentedControl) {
+    }
+    
+    @IBAction func graphButtonTapped(_ sender: Any) {
+        print("graph button tapped")
     }
     
 }
