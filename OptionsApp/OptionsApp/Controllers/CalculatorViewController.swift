5e578dcc787ec343690d9e39b8a9e42dafde724f
dmelloe <dailydmello@gmail.com>
2018-07-26 11:22:51 -0700

    Added Calculate Total Cost function

--- OptionsApp/OptionsApp/Controllers/CalculatorViewController.swift
+++ OptionsApp/OptionsApp/Controllers/CalculatorViewController.swift
@@ -18,8 +18,8 @@ class CalculatorViewController: UIViewController{
     @IBOutlet weak var currentPriceLabel: UILabel!
     
     @IBOutlet weak var getPriceButton: UIButton!
-    
-    @IBOutlet weak var optionStrategySegmentedControl: UISegmentedControl!
+
+    @IBOutlet weak var callPriceTextField: SymbolTextField!
     @IBOutlet weak var strikePriceTextField: SymbolTextField!
     @IBOutlet weak var numofContractsTextField: SymbolTextField!
     @IBOutlet weak var expiryDateTextField: SymbolTextField!
@@ -28,8 +28,65 @@ class CalculatorViewController: UIViewController{
     @IBOutlet weak var calculateCostButton: UIButton!
     @IBOutlet weak var graphButton: UIButton!
     
+//    var numofContracts: Float = 2
+//    var callPrice: Float = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //code enter underlying stock ticker, hit enter, and retrieve latest ticker stock price
+        symbolTextField.calculateButtonAction = {
+            self.getSymbolCurrentPrice()
+        }
+        
+        callPriceTextField.calculateButtonAction = {
+            if self.callPriceTextField.isFirstResponder {
+                self.callPriceTextField.resignFirstResponder()
+            }
+        }
+        strikePriceTextField.calculateButtonAction = {
+            if self.strikePriceTextField.isFirstResponder {
+                self.strikePriceTextField.resignFirstResponder()
+            }
+        }
+        numofContractsTextField.calculateButtonAction = {
+            if self.numofContractsTextField.isFirstResponder {
+                self.numofContractsTextField.resignFirstResponder()
+            }
+        }
+        expiryDateTextField.calculateButtonAction = {
+            if self.expiryDateTextField.isFirstResponder {
+                self.expiryDateTextField.resignFirstResponder()
+            }
+        }
+
+    }
+    @IBAction func getPriceButtonTapped(_ sender: UIButton) {
+        print("get price button tapped")
+    }
+
+    @IBAction func calculateCostButtonTapped(_ sender: UIButton) {
+        calculateCallTotalCost()
+    }
+    @IBAction func graphButtonTapped(_ sender: Any) {
+        print("graph button tapped")
+    }
+}
+
+extension CalculatorViewController{
+    func calculateCallTotalCost(){
+        //let numOfContracts: String
+        //let callPrice: Float
+        
+        guard let callPrice = self.callPriceTextField.text
+            else {return}
+        guard let numOfContracts = self.numofContractsTextField.text
+        else{return}
+        let totalnumOfCallOptions = Float(numOfContracts)! * 100
+        let totalCallCost = Float(callPrice)! * totalnumOfCallOptions
+        self.costLabel.text = String(totalCallCost)
+    }
+    
+    func getSymbolCurrentPrice(){
         //code enter underlying stock ticker, hit enter, and retrieve latest ticker stock price
         symbolTextField.calculateButtonAction = {
             guard let symbolText = self.symbolTextField.text
@@ -42,79 +99,44 @@ class CalculatorViewController: UIViewController{
             Alamofire.request(apiToContact).validate().responseJSON() { response in
                 var tempArr = [TimeSeriesIntraday]()
                 switch response.result {
-                    case .success:
-                        print ("API request was a success")
-                        //Get data ready to store in custom array of dictionaries
-                        let result = response.result.value as! [String:Any]
-                        guard let dict = result["Time Series (1min)"] as? [String: Any] else{
-                            print("Could not retrieve timestamps")
-                            //completion([])
-                            return
+                case .success:
+                    print ("API request was a success")
+                    //Get data ready to store in custom array of dictionaries
+                    let result = response.result.value as! [String:Any]
+                    guard let dict = result["Time Series (1min)"] as? [String: Any] else{
+                        print("Could not retrieve timestamps")
+                        //completion([])
+                        return
+                    }
+                    //Format timestamp to proper date format
+                    for dic in dict{
+                        guard let date = self.stringToDateAndTime(string: dic.key),
+                            let tempValue = dic.value as? [String: Any] else {
+                                return
                         }
-                        //Format timestamp to proper date format
-                        for dic in dict{
-                            guard let date = self.stringToDateAndTime(string: dic.key),
-                                let tempValue = dic.value as? [String: Any] else {
-                                    return
-                            }
                         //populate new array of TimeSeriestIntraday objects, 100 objects created 1 min apart
-                            tempArr.append(TimeSeriesIntraday(dict: tempValue, timeStamp: date))
-                        }
-                        //sort the array from oldest to newest data
-                        let sortedData = TimeSeriesIntraday.sortSeriesByTime(array: tempArr)
-                        guard let lastestTickerStockPrice = Double(sortedData[sortedData.count-1].close)
-                            else {return}
-                        let stockPriceRounded = (100 * lastestTickerStockPrice).rounded()/100
-                        self.currentPriceLabel.text = String(stockPriceRounded)
-                    case .failure(let error):
-                        print ("API request did not succeed")
-                        print(error)
+                        tempArr.append(TimeSeriesIntraday(dict: tempValue, timeStamp: date))
+                    }
+                    //sort the array from oldest to newest data
+                    let sortedData = TimeSeriesIntraday.sortSeriesByTime(array: tempArr)
+                    guard let latestTickerStockPrice = Float(sortedData[sortedData.count-1].close)
+                        else {return}
+                    let stockPriceRounded = (100 * latestTickerStockPrice).rounded()/100
+                    self.currentPriceLabel.text = String(stockPriceRounded)
+                case .failure(let error):
+                    print ("API request did not succeed")
+                    print(error)
                 }
             }
         }
-        strikePriceTextField.calculateButtonAction = {
-            guard let priceText = self.strikePriceTextField.text
-                else {return}
-            print(priceText)
-            if self.strikePriceTextField.isFirstResponder {
-                self.strikePriceTextField.resignFirstResponder()
-            }
-        }
-        numofContractsTextField.calculateButtonAction = {
-            guard let numofContractsText = self.numofContractsTextField.text
-                else {return}
-            print(numofContractsText)
-            if self.numofContractsTextField.isFirstResponder {
-                self.numofContractsTextField.resignFirstResponder()
-            }
-        }
-        expiryDateTextField.calculateButtonAction = {
-            guard let expiryDateText = self.expiryDateTextField.text
-                else {return}
-            print(expiryDateText)
-            if self.expiryDateTextField.isFirstResponder {
-                self.expiryDateTextField.resignFirstResponder()
-            }
-        }
     }
-    @IBAction func getPriceButtonTapped(_ sender: UIButton) {
-        print("get price button tapped")
-    }
-    @IBAction func strategySelected(_ sender: UISegmentedControl) {
-    }
-    @IBAction func calculateCostButtonTapped(_ sender: UIButton) {
-        print("calculate cost button tapped")
-    }
-    @IBAction func graphButtonTapped(_ sender: Any) {
-        print("graph button tapped")
-    }
-}
-
-extension CalculatorViewController{
+        
     func stringToDateAndTime(string: String) -> (Date?){
         let dateAndTimeFormatter = DateFormatter()
         dateAndTimeFormatter.dateFormat = "yyyy'-'MM'-'dd' 'HH':'mm':'ss"
         let dateAndTimeFormatted = dateAndTimeFormatter.date(from: string)
         return dateAndTimeFormatted
     }
+    
+    
 }
